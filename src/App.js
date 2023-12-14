3c2ce1f896a8deb019e88b5e1f5fd080182f0608
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2023-12-14 10:00:10 +0100

    debut books

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AdherentList from './views/adherent/AdherentList';
 import Result from "./views/scan/Result";
 import Scan from "./views/scan/scan";
 import HomepageAdmin from "./views/homepage/HomepageAdmin";
+import BooksList from "./views/books/BooksList";
 
 const App = () => {
     return (
@@ -15,7 +16,7 @@ const App = () => {
                 <Route path="/homepage" element={<HomepageAdmin/>} />
                 <Route path="/scan" element={<Result />} />
                 <Route path="/test" element={<Scan />} />
-                <Route path="/scan" element={<Result />} />
+                <Route path="/books" element={<BooksList />} />
             </Routes>
         </div>
     );
---
+++ src/components/NavBar.js
@@ -0,0 +1,63 @@
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="sans-titre-6-1-group">
+            <img className="sans-titre-6-11" alt="" src="/sans-titre6-1@2x.png" />
+            <div className="icons8-bibliothque-64-1-parent">
+                <img
+                    className="icons8-bibliothque-64-1"
+                    alt=""
+                    src="/icons8bibliothque64-1@2x.png"
+                />
+                <div className="group-parent3">
+                    <div className="group-parent4">
+                        <div className="group-parent5">
+                            <div className="group-icon">
+                                <img className="group-icon" alt="" src="/group-131.svg" />
+                                <div className="bibliothque-group">
+                                    <div className="bibliothque1">Bibliothèque</div>
+                                    <div className="bibliothque1">Bibliothèque</div>
+                                    <div className="mon-espace">Mon espace</div>
+                                    <Link to="/books">
+                                        <img
+                                            className="ionlibrary-icon1"
+                                            alt=""
+                                            src="/ionlibrary1.svg"
+                                        />
+                                    </Link>
+                                </div>
+                                <div className="accueil-group">
+                                    <div className="accueil2">Accueil</div>
+                                    <img
+                                        className="mingcutehome-2-fill-icon1"
+                                        alt=""
+                                        src="/mingcutehome2fill1.svg"
+                                    />
+                                </div>
+                            </div>
+                            <div className="silhouette-dutilisateurs-multi-parent">
+                                <Link to="/adherents">
+                                    <img
+                                        className="silhouette-dutilisateurs-multi-icon1"
+                                        alt=""
+                                        src="/silhouettedutilisateursmultiples-1@2x.png"
+                                    />
+                                </Link>
+                                <div className="adhrents2">Adhérents</div>
+                            </div>
+                        </div>
+                        <img className="group-child14" alt="" src="/group-1461.svg" />
+                    </div>
+                    <img
+                        className="icons8-bibliothque-64-11"
+                        alt=""
+                        src="/icons8bibliothque64-1@2x.png"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
