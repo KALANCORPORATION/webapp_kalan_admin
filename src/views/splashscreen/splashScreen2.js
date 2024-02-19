8096a73709c56c7ae40f8babb8202c9dfe9ed026
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2024-02-19 14:01:25 +0100

    link login button to the route

--- src/views/splashscreen/splashScreen2.js
+++ src/views/splashscreen/splashScreen2.js
@@ -1,7 +1,14 @@
 import "../../styles/splashscreen/splashScreen2.css";
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 
 const SplashScreen2 = () => {
+    const history = useNavigate();
+
+    const handleLoginClick = () => {
+        history(`/login`);
+    };
+
     return (
         <div className="welcome-screen">
             <div className="welcome-header">
@@ -11,7 +18,7 @@ const SplashScreen2 = () => {
             <div className="welcome-title">Bienvenue !</div>
             <img className="welcome-logo" src="logoKalan2.png" alt="KALAN" />
             <button className="welcome-button welcome-button-signup">S'inscrire</button>
-            <button className="welcome-button welcome-button-login">Se connecter</button>
+            <button className="welcome-button welcome-button-login" onClick={handleLoginClick}>Se connecter</button>
         </div>
     );
 };
