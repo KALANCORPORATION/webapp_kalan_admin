7f7fa941a7ab88e814dcd1b9eb33967baec50654
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2024-02-21 03:48:52 +0100

    scan in inscription adherent

--- src/views/adherent/Inscription.js
+++ src/views/adherent/Inscription.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../../styles/adherent/Inscription.css"
 import {Header} from "../../components/Header";
 import NavBarAdmin from "../../components/NavBarAdmin";
 import {useNavigate} from "react-router-dom";
 import SpaceController from "../../controllers/space/spaceController";
+import AdherentController from "../../controllers/adherent/adherentController";
+import styles from "../../styles/referent/List.module.css";
+import {QrReader} from "react-qr-reader";
 
 const InscriptionForm = () => {
     const navigate = useNavigate();
+    const [prenom, setPrenom] = useState('');
+    const [nom, setNom] = useState('');
+    const [dateNaissance, setDateNaissance] = useState('');
+    const [email, setEmail] = useState('');
+    const [telephone, setTelephone] = useState('');
+    const [scannedReferent, setScannedReferent] = useState(null);
+    const [isCameraOpen, setIsCameraOpen] = useState(false);
     const spaceId = localStorage.getItem('spaceId');
     const accessToken = localStorage.getItem('accessToken');
+
+    const populateFormWithAdherentData = (adherentData) => {
+        setPrenom(adherentData.first_name);
+        setNom(adherentData.last_name);
+        setDateNaissance(adherentData.birthday || '');
+        setEmail(adherentData.mail);
+        setTelephone(adherentData.phone_number);
+    };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -32,30 +50,78 @@ const InscriptionForm = () => {
         }
     };
 
+    const handleScan = async (data) => {
+        if (data) {
+            let cleanedText = data.text.replace(/@/g, '');
+            try {
+                const qrData = JSON.parse(cleanedText);
+                if (qrData && qrData.user_id) {
+                    const adherentData = await AdherentController.getAdherentById(qrData.user_id, accessToken);
+                    if (adherentData) {
+                        populateFormWithAdherentData(adherentData);
+                        setIsCameraOpen(false);
+                    }
+                }
+            } catch (error) {
+                console.error('Error processing QR code:', error);
+            }
+        }
+    };
+
+    const startScanning = () => {
+        setIsCameraOpen(true);
+    };
+
+    const handleError = (error) => {
+        console.error(error);
+    };
+
+    const getTabStyle = (path) => {
+        return location.pathname === path ? `${styles.tab} ${styles.active}` : styles.tab;
+    };
+
+    const closeCameraPopup = () => {
+        setIsCameraOpen(false);
+    };
+
     return (
         <div className="inscription-container">
             <Header />
 
             <form className="inscription-form" onSubmit={handleSubmit}>
                 <label htmlFor="prenom">Prénom*</label>
-                <input type="text" id="prenom" name="prenom" required />
+                <input type="text" id="prenom" name="prenom" required value={prenom} onChange={e => setPrenom(e.target.value)} />
 
                 <label htmlFor="nom">Nom*</label>
-                <input type="text" id="nom" name="nom" required />
+                <input type="text" id="nom" name="nom" required value={nom} onChange={e => setNom(e.target.value)} />
 
                 <label htmlFor="date">Date de naissance*</label>
-                <input type="date" id="date" name="date" required />
+                <input type="date" id="date" name="date" required value={dateNaissance} onChange={e => setDateNaissance(e.target.value)} />
 
                 <label htmlFor="email">Mail</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
 
                 <label htmlFor="telephone">Téléphone (facultatif)</label>
-                <input type="tel" id="telephone" name="telephone" />
+                <input type="tel" id="telephone" name="telephone" value={telephone} onChange={e => setTelephone(e.target.value)} />
 
                 <div className="submit-section">
                     <button type="submit" className="submit-button">Ajouter</button>
+                    <img src="/img_4.png" alt="Scan QR Code" onClick={startScanning} className="qr-code-icon" />
                 </div>
             </form>
+
+            {isCameraOpen && (
+                <div className={styles.cameraPopup}>
+                    <QrReader
+                        delay={50}
+                        constraints={{ facingMode: 'environment', focusMode: 'continuous'}}
+                        onResult={handleScan}
+                        onError={handleError}
+                        style={{ width: '100%' }}
+                    />
+                    <button onClick={closeCameraPopup}>Fermer</button>
+                </div>
+            )}
 
             <NavBarAdmin />
         </div>
